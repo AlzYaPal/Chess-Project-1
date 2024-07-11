6a3164322c4b79503c903b0086fa55fc5730fb6b
AlzYaPal <139356554+AlzYaPal@users.noreply.github.com>
2024-07-11 12:33:07 +0100

    Added beginning of Vars Class

---
+++ src/vars.py
@@ -0,0 +1,17 @@
+import pygame
+pygame.init()
+
+class Vars:
+    def __init__(self):
+        self.board = [
+            "bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR",
+            "bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp",
+            "--", "--", "--", "--", "--", "--", "--", "--",
+            "--", "--", "--", "--", "--", "--", "--", "--",
+            "--", "--", "--", "--", "--", "--", "--", "--",
+            "--", "--", "--", "--", "--", "--", "--", "--",
+            "wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp",
+            "wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"
+        ]
+        self.whiteToMove = True
+        
